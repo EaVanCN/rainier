f9a710e4df76b7f21771e178b932dbd1d6ffa9ad
EaVanCN <pengyifan93@live.com>
2017-11-09 15:15:29 +0800

    MVVM模块相关

--- js/rainier.MVVM.js
+++ js/rainier.MVVM.js
@@ -15,35 +15,35 @@ Rainier.prototype.init = function(option){
 }
 
 Rainier.prototype.compiler = function(){         //将页面上指令进行提取,将{{}}转换为指令，并用span包裹。（预处理阶段），获取_data,DOM元素，指令的对应,(v-model在这个地方做对应？)
+    var self = this;
     var span, spans, textEle, vm_name, otherEles;           //spans表示ra-textNode选到的所有文本节点，span用于spans的循环；textEle表示创建的文本节点，vm_name表示this._data中数据的名称;otherEles表示页面其他指令部分
     for (var i = 0, length = this._ele.length; i < length; i++){
         var ele = this._ele[i], html = ele.innerHTML;
         html = html.replace(/\{\{(.*?)\}\}/g, function(a, b){
-            return '<span ra-type="textNode" ra-textNode="'+ b +'"></span>';
+            if(self._data.hasOwnProperty(b)){
+                return '<span ra-type="textNode" ra-textNode="'+ b +'"></span>';
+            }else{
+                return '{{'+ b +'}}'
+            }
         });
         ele.innerHTML = html;
     };
-    //ra-text中的值和ra-textNode中需要创建新的文本节点，才能够获得节点、viewmodel的对应关系，所以直接在这里创建，并移除指令。
-    spans = document.querySelectorAll("[ra-textNode],[ra-text]");
+    spans = document.querySelectorAll("[ra-textNode],[ra-text]");    //ra-text中的值和ra-textNode中需要创建新的文本节点，才能够获得节点、viewmodel的对应关系，所以直接在这里创建，并移除指令。
     for (var j = 0, l = spans.length; j < l; j++) {
         span = spans[j];
         vm_name = span.getAttribute('ra-textNode') || span.getAttribute('ra-text');
         if(!this.reactData[vm_name]) {
-            this.reactData[vm_name] = {value: this._data[vm_name] || "", ele: []};
+            this.reactData[vm_name] = [];
         }
         textEle = document.createTextNode(this._data[vm_name]);
         if(span.getAttribute("ra-type")){                                       //判断是新增的文本节点，还是通过ra-text指令产生的文本节点
-            this.reactData[vm_name].ele.push({node:textEle,cmd:"ra-textNode"});
+            this.reactData[vm_name].push({node:textEle,cmd_key:"ra-textNode",cmd_val:this._data[vm_name]});
             span.parentNode.replaceChild(textEle,span);
         }else{
-            this.reactData[vm_name].ele.push({node:span,cmd:"ra-text"});
+            this.reactData[vm_name].push({node:span,cmd_key:"ra-text",cmd_val:this._data[vm_name]});
             span.appendChild(textEle);
             span.removeAttribute('ra-text');
         }
-    }
-    otherEles = document.querySelectorAll("[ra-html],[ra-model],[ra-for],[ra-if]");     //选择其他指令
-    for (var m = 0; m < otherEles.length; m++){
-
     }
 }
 
